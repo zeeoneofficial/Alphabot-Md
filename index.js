69c09693a1ed662b34e1bf239cfa28be3779f9cd
Official Acc Of Zeeone Ofc <100220802+zeeone-ofc@users.noreply.github.com>
2022-05-14 09:58:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@
 
 
 require('./settings')
-const { default: makeWASocket, BufferJSON, WAMessageStubType, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia , generateWAMessage, areJidsSameUser, makeInMemoryStore} = require('@adiwajshing/baileys-md')
+const { default: makeWASocket, BufferJSON, WAMessageStubType, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia , generateWAMessage, areJidsSameUser, makeInMemoryStore} = require('@adiwajshing/baileys')
 const translate = require('@vitalets/google-translate-api')
 const fs = require('fs')
 const util = require('util')
